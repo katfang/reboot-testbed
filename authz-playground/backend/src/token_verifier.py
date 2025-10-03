ea1e7ed1dc01dc9e575cfb7b468d377a5e9e9177
Kat Fang <katfang@users.noreply.github.com>
2025-10-03 16:59:13 +0000

    importing optional

--- authz-playground/backend/src/token_verifier.py
+++ authz-playground/backend/src/token_verifier.py
@@ -1,13 +1,14 @@
 from rebootdev.aio.auth import Auth, token_verifiers
 from rebootdev.aio.contexts import ReaderContext
+from typing import Optional
 
 class TokenVerifier(token_verifiers.TokenVerifier):
 
     async def verify_token(
         self,
         context: ReaderContext,
-        token: str | None,
-    ) -> Auth | None:
+        token: Optional[str],
+    ) -> Optional[Auth]:
         print("!!! I have a token", token)
 
         return Auth(
